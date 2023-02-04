e8db07adf0fdb88c115715b03491ac38fef47bd4
isabellequaye <52260763+isabellequaye@users.noreply.github.com>
2023-02-04 17:53:37 -0500

    Streamlined code and made some changes

--- tutor.py
+++ tutor.py
@@ -1,14 +1,15 @@
 import student
 class Tutor(student.Student):
-    def __init__(self,keberos,name,subject_set, generics_set, hours) -> None:
+    def __init__(self,keberos,name,subject_set, hours) -> None:
         super().__init__(keberos,name)
         self.subjects = subject_set
-        self.generics = generics_set # specifies a broad category of classes eg. 7.01x, 6.00x
         self.hours = hours
         self.assigned_hours = 0
         self.assigned_students_and_subject = {}
+        self.received_assignment = False
     
     def assign_hours(self, hours, student, subject):
+        self.received_assignment = True
         self.assigned_hours += hours
         if student in self.assigned_students_and_subject:
             subjects_and_hours = self.assigned_students_and_subject[student]
@@ -16,7 +17,6 @@ class Tutor(student.Student):
                 subjects_and_hours[subject] += hours
             else:
                 subjects_and_hours[subject] = hours
-
         else:
             self.assigned_students_and_subject[student] = {subject:hours}
     
